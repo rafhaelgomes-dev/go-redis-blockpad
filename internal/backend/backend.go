1fae536ff459acdad1153b4e34d14cb3930e2725
Rafhael Gomes <rafhaelgomes.dev@gmail.com>
2024-02-24 14:55:11 -0300

    first commit

---
+++ internal/backend/backend.go
@@ -0,0 +1,42 @@
+package backend
+
+import (
+	"blocopad/internal/db"
+	"errors"
+)
+
+func GetKey(key string) (string, error) {
+	if len(key) == 0 || len(key) > 36 {
+		return "", errors.New("key with wrong size")
+	}
+
+	oneTime, data, err := db.GetNote(key)
+
+	if err != nil {
+		return "", err
+	}
+
+	if oneTime {
+		if err := db.DeleteNote(key); err != nil {
+			panic("Cannot delete onetime note!!!!")
+		}
+	}
+
+	return data, nil
+}
+
+func SaveKey(data string, oneTime bool) (string, error) {
+	byteSize := len([]rune(data))
+
+	if byteSize == 0 || byteSize > (32*1024) {
+		return "", errors.New("invalid note size")
+	}
+
+	uuidCode, err := db.SaveNote(data, oneTime)
+
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+
+	return uuidCode, nil
+}
